fd5cf278137e3cffe232b698ab994503d9a158c5
Chih-Yuan Su <csu68@gatech.edu>
2021-05-02 13:28:46 -0700

    Added config class

---
+++ original/config.py
@@ -0,0 +1,13 @@
+class CONFIG(object):
+    """docstring for CONFIG"""
+    def __init__(self):
+        super(CONFIG, self).__init__()
+        self.dataset = '20ng'
+        self.model = 'gcn'  # 'gcn', 'dense'
+        self.learning_rate = 0.02  # Initial learning rate.
+        self.epochs = 200  # Number of epochs to train.
+        self.hidden1 = 200  # Number of units in hidden layer 1.
+        self.dropout = 0.5  # Dropout rate (1 - keep probability).
+        self.weight_decay = 0.  # Weight for L2 loss on embedding matrix.
+        self.early_stopping = 10  # Tolerance for early stopping (# of epochs).
+        self.max_degree = 3  # Maximum Chebyshev polynomial degree.
